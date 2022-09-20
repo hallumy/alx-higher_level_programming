6ac97c903b1c918084a2d2fa54cca00f86a8830d
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-09-20 17:40:18 +0300

    Correction on task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#function to print alphabet in lowercase
 for alpha in range(97, 122):
-    print("{:s}" .format(chr(alpha)), end = "")
+    print("{:s}".format(chr(alpha)), end="")
