d166732fb7e0cd6151c8745a612e637c549674fe
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-09-22 20:35:34 +0300

    A program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+def total(argv):
+    length = len(argv) - 1
+    if length == 0:
+        print("{:d}".format(length))
+        return
+    else:
+        index = 1
+        totals = 0
+        while index <= length:
+            totals += int(argv[index])
+            index += 1
+        print("{:d}".format(totals))
+if __name__ == "__main__":
+    total(argv)
