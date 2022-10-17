54503cfc14d600b8d2cec67a43ef0bf2d933004a
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-10-17 20:44:22 +0300

    Defining a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,18 +14,6 @@ class Rectangle:
         self.height = height
 
         @property
-        def width(self):
-            """Set the current width of the rectangle"""
-            return (self.__width)
-
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width =value
-        @property
         def height(self):
             """Set the current height of the rectangle"""
             return (self.__height)
@@ -37,3 +25,14 @@ class Rectangle:
             elif value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
+        @property
+        def width(self):
+            """Set the current width of the rectangle"""
+            return (self.__width)
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width =value
