6f8d01ef5691734ca6f5940792d8a883fbe160fe
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-12-20 08:49:57 +0300

    Correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.slice(2).length === 1) {
-  console.log('Argument found');
-} else if (process.argv.slice(2).length > 1) {
+if (process.argv.slice(2).length > 1) {
   console.log('Arguments found');
+} else if (process.argv.slice(2).length === 1) {
+  console.log('Argument found');
 } else {
   console.log('No Argument');
 }
